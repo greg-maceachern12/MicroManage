00abe2b3c72deb9935fad320e3f0641bd5c8b731
Michael Briggs <michaelbriggs@Michaels-iMac.local>
2018-10-23 10:37:39 -0400

    Adding side bar functionality to Messages Page

--- King_Project/messages.cpp
+++ King_Project/messages.cpp
@@ -5,4 +5,7 @@ Messages::Messages(QWidget *parent) : QWidget(parent), ui(new Ui::Messages)
     ui->setupUi(this);
     ui->category->addItem("Sent");
     ui->category->addItem("Received");
+    ui->toolButton->setCheckable(true);
+    ui->toolButton->setText("Menu");
+    connect(ui->toolButton, SIGNAL(clicked()), parent, SLOT(showSideMenu()));
 }
