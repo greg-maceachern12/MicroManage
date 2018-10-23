08011d84df6cead39ca3bb267e589acdefa63743
Michael Briggs <michaelbriggs@Michaels-iMac.local>
2018-10-23 10:44:57 -0400

    Added toolbar functionality to all pages

--- King_Project/profile.cpp
+++ King_Project/profile.cpp
@@ -4,6 +4,9 @@
 Profile::Profile(QWidget *parent) :QWidget(parent),ui(new Ui::Profile)
 {
     ui->setupUi(this);
+    ui->toolButton->setCheckable(true);
+    ui->toolButton->setText("Menu");
+    connect(ui->toolButton, SIGNAL(clicked()), parent, SLOT(showSideMenu()));
     //ui->toolButton->setText("Menu");
 
     // Create the button, make "this" the parent
--- King_Project/settings.cpp
+++ King_Project/settings.cpp
@@ -2,4 +2,7 @@
 
 Settings::Settings(QWidget *parent) : QWidget(parent), ui(new Ui::Settings) {
     ui->setupUi(this); // Sets up the .ui file GUI
+    ui->toolButton->setCheckable(true);
+    ui->toolButton->setText("Menu");
+    connect(ui->toolButton, SIGNAL(clicked()), parent, SLOT(showSideMenu()));
 }
