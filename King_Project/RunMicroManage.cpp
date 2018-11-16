e018b2b55bc290e8903d9117a5d785f900392697
Lauren Bhagwandat <laurenbhagwandat@Macintosh.local>
2018-11-16 15:50:39 -0500

    Created a master QSS style sheet for the app.

--- King_Project/RunMicroManage.cpp
+++ King_Project/RunMicroManage.cpp
@@ -4,6 +4,10 @@
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     a.setAttribute(Qt::AA_UseHighDpiPixmaps);
+    QFile styleFile(":css/master.qss");
+    styleFile.open(QFile::ReadOnly);
+    QString styleSheet = QLatin1String(styleFile.readAll());
+    a.setStyleSheet(styleSheet);
     MicroManage w;
     w.show();
 
