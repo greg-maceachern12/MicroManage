976d3c68ae68c92fa78058a53d15b0cdb6c3d863
chen <15xc16@caslab.queensu.ca>
2018-11-20 00:25:09 -0500

    update

--- King_Project/settings.cpp
+++ King_Project/settings.cpp
@@ -6,10 +6,15 @@ Settings::Settings(QWidget *parent) : QWidget(parent), ui(new Ui::SettingsForm) 
     ui->menuButton->setIcon(QIcon(":images/icons/menu_icon.png"));
     ui->menuButton->setIconSize(QSize(25, 25));
     connect(ui->menuButton, SIGNAL(clicked()), parent, SLOT(showSideMenu()));
+    connect(ui->questionButton, SIGNAL(clicked()), parent, SLOT(emailLink()));
 }
 
 QToolButton* Settings::getMenuButton() {
     return ui->menuButton;
+}
+
+void emailLink(){
+    QDesktopServices::openUrl(QUrl("mailto:15xc16@queensu.ca"));
 }
 
 Settings::~Settings() {
